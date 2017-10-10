494e6b0163dcddc4689371c62bd0dcdc9a2e2903
ybyGold <415098820@qq.com>
2017-10-10 14:34:58 -0700

    no message

--- app/src/main/java/com/example/yuboyang/ilovemovie1/network/MovieWebService.java
+++ app/src/main/java/com/example/yuboyang/ilovemovie1/network/MovieWebService.java
@@ -4,6 +4,7 @@ import com.example.yuboyang.ilovemovie1.MoviesWrapper;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * Created by yuboyang on 10/8/17.
@@ -11,6 +12,16 @@ import retrofit2.http.GET;
 
 public interface MovieWebService {
 
-    @GET("3/discover/movie?language=en?sort_by=popularity.desc")
+    @GET("3/discover/movie?language=en&sort_by=popularity.desc")
     Observable<MoviesWrapper> popularMovies();
+
+    @GET("3/discover/movie?vote_count.gte=500&language=en&sort_by=vote_average.desc")
+    Observable<MoviesWrapper> highestRatedMovies();
+
+    @GET("3/movie/{movieId}/videos")
+    Observable<MoviesWrapper> trailers(@Path("movieId") String movieId);
+
+    @GET("3/movie/{movieId}/reviews")
+    Observable<MoviesWrapper> reviews(@Path("movieId") String movieId);
+
 }
